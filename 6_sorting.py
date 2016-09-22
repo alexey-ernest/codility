476c12828b8d64163acde69e689bd06e73331caa
alexey-ernest <alexey.ernest@gmail.com>
2016-09-22 12:07:23 +0300

    Merge sort

--- 6_sorting.py
+++ 6_sorting.py
@@ -31,4 +31,44 @@ def countingSort(A, k):
 
   return A
 
-print countingSort([1,7,3,5,2], 7)
+# Split current array into two parts, sort each part and merge sorted arrays. 
+# Time complexity - O(nlogn), space complexity - O(n)
+def mergeSort(A):
+  n = len(A)
+
+  if (n < 2):
+    # one element array is already sorted
+    return A
+  
+  # sorting halfs
+  middle = n // 2
+  left = mergeSort(A[:middle])
+  right = mergeSort(A[middle:])
+
+  # merging result
+  i = j = 0
+  llen = len(left)
+  rlen = len(right)
+  tail = []
+  for k in xrange(n):
+    if i >= llen:
+      tail = right[j:]
+      break
+    if j >= rlen:
+      tail = left[i:]
+      break
+
+    if left[i] < right[j]:
+      A[k] = left[i]
+      i += 1
+    else:
+      A[k] = right[j]
+      j += 1
+
+  # appending tail
+  for i in xrange(k, n):
+    A[i] = tail[i - k]
+
+  return A
+
+print mergeSort([1,7,3,5,2,2,8,6])
