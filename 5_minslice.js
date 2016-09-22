e24191e9aec5cf0b9fec560980b5bfd49fd150b1
alexey-ernest <alexey.ernest@gmail.com>
2016-09-22 09:44:44 +0300

    Brilliant min avg slice task

--- 5_minslice.js
+++ 5_minslice.js
@@ -1,52 +1,39 @@
-
-function sum(P, i, j) {
-  return P[j + 1] - P[i];
-}
-
+/**
+ * Finds a slice of A with minimum average.
+ * The important thing that we only need to check 2- and 3-length
+ * slices. Because any slice of length > 3 can be represented as
+ * an average of 2- and 3-length slices, that means that there
+ * should be 2- or 3-length slice which has the minimum avg
+ * (slice of length > 3 always has avg >= smaller slices).
+ *
+ * @param      {Array}  A       Returns index of the min avg slice.
+ */
 function minslice(A) {
-    var len = A.length,
-      i,
-      prefsums = [0];
-    for (i = 0; i < len; i+=1) {
-      prefsums[i + 1] = prefsums[i] + A[i];
+  var len = A.length,
+      avg,
+      avg2,
+      avg3,
+      minAvg = Number.POSITIVE_INFINITY,
+      minIndex = 0,
+      i;
+  for (i = 0; i < len - 2; i+=1) {
+    avg2 = (A[i] + A[i + 1]) / 2;
+    avg3 = (A[i] + A[i + 1] + A[i + 2]) / 3;
+    avg = Math.min(avg2, avg3);
+    if (avg < minAvg) {
+      minAvg = avg;
+      minIndex = i;
     }
-    
-    var minAvg = Number.MAX_VALUE,
-        minIndex = 0,
-        avg,
-        avg1,
-        avg2,
-        j,
-        k1,
-        k2;
+  }
 
-    i = 0;
-    j = len - 1;
-    while ((j - i + 1) > 2) {
-      avg = sum(prefsums, i, j) / (j - i + 1);
-      if (avg < minAvg) {
-        // memorizing first minimum avg (< sign is crutial)
-        minAvg = avg;
-        minIndex = i;
-      }
+  // checking tail (n-2, n-1)
+  avg = (A[i] + A[i + 1]) / 2;
+  if (avg < minAvg) {
+    minAvg = avg;
+    minIndex = i;
+  }
 
-      // determining wheter to go: to the left or to the right
-      // for that we will compare avg for left and right halfs
-      k1 = Math.floor(i + (j - i) / 2);
-      k2 = Math.ceil(i + (j - i) / 2);
-      avg1 = sum(prefsums, i, k1) / (k1 - i + 1);
-      avg2 = sum(prefsums, k2, j) / (j - k2 + 1);
-
-      if (avg1 <= avg2) {
-        // left half avg is smaller, decrementing right bound then
-        j--;
-      } else {
-        // incrementing left bound
-        i++;
-      }
-    }
-
-    return minIndex;
+  return minIndex;
 }
 
 module.exports = minslice;
