b60d8789072a040ce5815eb966ab7480608a9177
alexey-ernest <alexey.ernest@gmail.com>
2016-09-26 01:12:44 +0300

    More tests

--- 9_doublemaxslice.spec.js
+++ 9_doublemaxslice.spec.js
@@ -3,5 +3,8 @@ var assert = require('assert');
 var doublemaxslice = require('./9_doublemaxslice');
 
 assert(doublemaxslice([3,2,6,-1,4,5,-1,2]) === 17);
+assert(doublemaxslice([-1,0,1,-1,-1,1,0,1,-1]) === 2);
+assert(doublemaxslice([-1,1,1,-1,-1,1,1,-1]) === 3);
+assert(doublemaxslice([-1,1,1,-1,-1,1,1,-1,-1,-1,-1,-1,1,1,1,1,1,1]) === 5);
 
 // random numbers from -1 to 1, length = ~100,000 (got 455 expected 456)
