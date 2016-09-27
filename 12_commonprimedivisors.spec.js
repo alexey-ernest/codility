39266a75d9fb205743d86c0c6cc588b18f29e117
alexey-ernest <alexey.ernest@gmail.com>
2016-09-27 12:28:46 +0300

    New test

--- 12_commonprimedivisors.spec.js
+++ 12_commonprimedivisors.spec.js
@@ -3,3 +3,4 @@ var commonprimedivisors = require('./12_commonprimedivisors');
 
 assert(commonprimedivisors([15, 10, 9], [75, 30, 5]) === 1);
 assert(commonprimedivisors([1], [1]) === 1);
+assert(commonprimedivisors([2, 1, 2], [1, 2, 2]) === 1);
