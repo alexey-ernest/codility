ec73b517c2104443d9f91f3e88a7f8a3de04b5d3
alexey-ernest <alexey.ernest@gmail.com>
2016-09-27 12:38:25 +0300

    Fibonacci dynamic

---
+++ 13_fibonacci.py
@@ -0,0 +1,12 @@
+def fibonacciDynamic(n):
+  """
+  @brief      Fibonacci dynamic algorithm: O(n)
+  """
+  fib = [0] * (n + 1)
+  fib[1] = 1
+  for i in xrange(2, n + 1):
+    fib[i] = fib[i - 1] + fib[i - 2];
+
+  return fib[n];
+
+assert fibonacciDynamic(10) == 55
